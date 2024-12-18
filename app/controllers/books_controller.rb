7c8e1453d98d790e813f25b2c45c2ca4209b8b24
EC2 Default User <ec2-user@ip-172-31-32-171.ap-northeast-1.compute.internal>
2024-12-18 11:28:20 +0000

    [Finish]user-edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,14 +8,29 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path(params[:id])
+    redirect_to book_path(@book.id)
   end
 
   def show
+    @books = Book.find(params[:id])
+    @user = User.find(@books.user_id)
+    @book = Book.new
+  end
+
+  def destroy
     @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
+  def index
+    @user = User.all
+    @book = Book.new
+  end
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
   end
 
   def edit
+    @user = User.find(current_user.id)
+  end
+
+  def update
+    @user = User
   end
 end
